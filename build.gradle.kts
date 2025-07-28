b8632f6a8aa594ea940b9b892eb56fe86b8dd70e
MultiDev <2815968613@qq.com>
2025-07-28 18:22:07 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    extra["kotlin_version"] = "2.1.21"
+    
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.11.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
