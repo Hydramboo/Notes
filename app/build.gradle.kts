ed6f1b01eb2f8895d71e1ad77c7ba5ee2624f22d
MultiDev <2815968613@qq.com>
2025-07-28 18:25:57 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 }
 
 android {
-    compileSdkVersion(28)
+    compileSdkVersion = 35
     defaultConfig {
         applicationId = "com.petersommerhoff.kudoofinal"
-        minSdkVersion(19)
-        targetSdkVersion(28)
+        minSdkVersion = 16
+        targetSdkVersion = 26
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
@@ -26,29 +26,25 @@ android {
 dependencies {
     val kotlin_version: String by rootProject.extra
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
     implementation("com.android.support:appcompat-v7:28.0.0")
     implementation("com.android.support.constraint:constraint-layout:1.1.3")
     implementation("com.android.support:design:28.0.0")
 
-    val room_version = "1.1.1"  // Adjust to newest version if you want to
+    val room_version = "+"  // Adjust to newest version if you want to
     implementation("android.arch.persistence.room:runtime:$room_version")
     kapt("android.arch.persistence.room:compiler:$room_version")
 
-    val coroutines_version = "1.2.1"
+    val coroutines_version = "+"
     // Use newest version if you want (might differ)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
 
-    val lifecycle_version = "1.1.1"
+    val lifecycle_version = "+"
     implementation("android.arch.lifecycle:extensions:$lifecycle_version")
     kapt("android.arch.lifecycle:compiler:$lifecycle_version")
 
     testImplementation("junit:junit:4.12")
     androidTestImplementation("com.android.support.test:runner:1.0.2")
     androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
-}
-
-androidExtensions {
-    isExperimental = true
 }
